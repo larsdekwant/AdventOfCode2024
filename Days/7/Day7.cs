7d9f4e12fd749156ada696d68f4e0a00f66aadb1
larsdekwant <Larskwant@gmail.com>
2024-12-07 13:17:45 +0100

    clean up day 7

--- Days/7/Day7.cs
+++ Days/7/Day7.cs
@@ -16,39 +16,44 @@ namespace AdventOfCode
                 long goal = long.Parse(vals[0]);
                 var nums = vals.Skip(1).Select(long.Parse).ToArray();
 
+                if (TryOperators([Multiply, Sum], goal, nums)) total += goal;
+            }
+
+            return total;
+        }       
+
+        public long RunPart2()
+        {
+            var input = File.ReadLines("../../../Days/7/InputPart1.txt");
+
+            long total = 0;
+            foreach (var line in input)
+            {
+                string[] vals = line.Split([':', ' '], StringSplitOptions.RemoveEmptyEntries);
+                long goal = long.Parse(vals[0]);
+                var nums = vals.Skip(1).Select(long.Parse).ToArray();
+
                 if (TryOperators([Multiply, Sum, Concat], goal, nums)) total += goal;
             }
 
             return total;
         }
 
-        private bool TryOperators(Func<long, long, long>[] ops, long goal, long[] nums)
-        {
-            if (nums.Length < 2) return false;
-            return ops.Any(op => TryOperator(op, ops, goal, nums[0], nums, 1));
-        }
+        // Start recursion by just summing 0 + number1 (does nothing)
+        private bool TryOperators(Func<long, long, long>[] ops, long goal, long[] nums) => 
+            TryOperator(Sum, ops, goal, 0, nums, 0);
 
-        private bool TryOperator(Func<long, long, long> op, Func<long, long, long>[] ops, long goal, long x, long[] rest, int restIndex)
+        private bool TryOperator(Func<long, long, long> op, Func<long, long, long>[] ops, long goal, long x, long[] nums, int nextI)
         {
-            if (restIndex == rest.Length) 
-            {
-                return x == goal;
-            }
+            if (x > goal) return false;
+            if (nextI == nums.Length) return x == goal;
 
-            long result = op(x, rest[restIndex]);
-            return ops.Any(op => TryOperator(op, ops, goal, result, rest, restIndex + 1));
+            long result = op(x, nums[nextI]);
+            return ops.Any(op => TryOperator(op, ops, goal, result, nums, nextI + 1));
         }
 
         private long Multiply(long x, long y) => x * y;
         private long Sum(long x, long y) => x + y;
-        private long Concat(long x, long y) => long.Parse(x.ToString() + y.ToString());
-
-
-
-        public long RunPart2()
-        {
-            string[] input = File.ReadLines("../../../Days/6/InputPart1.txt").ToArray();
-            return 0;
-        }                
-    }  
+        private long Concat(long x, long y) => long.Parse($"{x}{y}");
+    }
 }
