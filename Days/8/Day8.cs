2cc3a104e6cd2eb0eaffc6bc85675b30045c0699
larsdekwant <Larskwant@gmail.com>
2024-12-08 21:12:19 +0100

    merge antinode functions for part1 and part2 into one

--- Days/8/Day8.cs
+++ Days/8/Day8.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
-using System.Xml.Linq;
+using System.Numerics;
 
 namespace AdventOfCode
 {
@@ -15,19 +14,19 @@ namespace AdventOfCode
             char[,] map = new char[rows, cols];
 
             // Group positions of nodes of the same frequency together in lists
-            Dictionary<char, List<(int x, int y)>> freqLocMap = new Dictionary<char, List<(int x, int y)>>();           
+            Dictionary<char, List<Vector2>> freqLocMap = new Dictionary<char, List<Vector2>>();           
             for (int y = 0; y < rows; y++)
                 for (int x = 0; x < cols; x++)
                 {
                     char val = input[y][x];
                     map[x, y] = val;
-                    if(val != '.' && !freqLocMap.TryAdd(val, [(x, y)]))
+                    if(val != '.' && !freqLocMap.TryAdd(val, [new(x, y)]))
                     {
-                        freqLocMap[val].Add((x, y));
+                        freqLocMap[val].Add(new(x, y));
                     }                    
                 }
 
-            HashSet<(int x, int y)> antinodes = [];
+            HashSet<Vector2> antinodes = [];
             foreach (var nodes in freqLocMap.Values)           
                 antinodes.UnionWith(GetFreqAntinodes(part, map, nodes));          
 
@@ -35,51 +34,44 @@ namespace AdventOfCode
         }
 
         // Loop over all pairs of nodes, compute their distances and place an antinode 1 distance away from the freq nodes.
-        private HashSet<(int x, int y)> GetFreqAntinodes(int part, char[,] map, List<(int x, int y)> nodes)
+        private HashSet<Vector2> GetFreqAntinodes(int part, char[,] map, List<Vector2> nodes)
         {
-            HashSet<(int x, int y)> antinodes = [];
+            HashSet<Vector2> antinodes = [];
             for (int i = 0; i < nodes.Count; i++)            
                 for (int j = i + 1; j < nodes.Count; j++)
                 {
-                    (int x, int y) n1 = nodes[i];
-                    (int x, int y) n2 = nodes[j];
+                    Vector2 n1 = nodes[i];
+                    Vector2 n2 = nodes[j];
 
-                    (int x, int y) diff1 = (n1.x - n2.x, n1.y - n2.y);
-                    (int x, int y) diff2 = (n2.x - n1.x, n2.y - n1.y);
+                    Vector2 diff1 = n1 - n2;
+                    Vector2 diff2 = n2 - n1;
 
                     switch (part)
                     {
                         case 1:
-                            AddSingleAntinode(map, antinodes, n1, diff1);
-                            AddSingleAntinode(map, antinodes, n2, diff2);
+                            AddAntinodes(map, antinodes, n1 + diff1);
+                            AddAntinodes(map, antinodes, n2 + diff2);
                             break;
                         case 2:
-                            AddAntinodesInDirection(map, antinodes, n1, diff1);
-                            AddAntinodesInDirection(map, antinodes, n2, diff2);
+                            AddAntinodes(map, antinodes, n1, diff1, line: true);
+                            AddAntinodes(map, antinodes, n2, diff2, line: true);
                             break;
                         default:
                             throw new ArgumentException("Not a valid part");
                     }                   
                 }          
             return antinodes;
-        }
-
-        // Adds a single antinode in a direction after checking bounds.
-        private void AddSingleAntinode(char[,] map, HashSet<(int x, int y)> antinodes, (int x, int y) anti, (int x, int y) diff)
-        {
-            anti = (anti.x + diff.x, anti.y + diff.y);
-            if (anti.x >= 0 && anti.x < map.GetLength(0) && anti.y >= 0 && anti.y < map.GetLength(1)) 
-                antinodes.Add(anti);        
-        }
+        }       
 
         // Adds antinodes to the set in a certain direction, as long as they are in bounds.
-        private void AddAntinodesInDirection(char[,] map, HashSet<(int x, int y)> antinodes, (int x, int y) anti, (int x, int y) diff)
+        private void AddAntinodes(char[,] map, HashSet<Vector2> antinodes, Vector2 anti, Vector2 diff = default, bool line = false)
         {
-            while (anti.x >= 0 && anti.x < map.GetLength(0) && anti.y >= 0 && anti.y < map.GetLength(1))
+            do
             {
+                if (anti.X < 0 || anti.X >= map.GetLength(0) || anti.Y < 0 || anti.Y >= map.GetLength(1)) return;
                 antinodes.Add(anti);
-                anti = (anti.x + diff.x, anti.y + diff.y);
-            }
+                anti += diff;
+            } while (line);          
         }
-    }            
+    }   
 }
