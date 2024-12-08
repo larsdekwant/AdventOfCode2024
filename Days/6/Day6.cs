c126179f65eeeca9ffeeb58ce30bf89b46d15a22
larsdekwant <Larskwant@gmail.com>
2024-12-08 12:20:24 +0100

    refactor day 6 code to reuse more code

--- Days/6/Day6.cs
+++ Days/6/Day6.cs
@@ -4,23 +4,17 @@ namespace AdventOfCode
 {
     class Day6 : IDay<int>
     {
-        const int STRAIGHT = 0;
-        const int UP = -1;
+        const int STRAIGHT =  0;
+        const int UP       = -1;    
 
-        readonly int rows;
-        readonly int cols;
-        char[,] map;
-
-        readonly int startX;
-        readonly int startY;
-
-        public Day6()
+        public int RunPart(int part)
         {
             string[] input = File.ReadLines("../../../Days/6/InputPart1.txt").ToArray();
-            rows = input.Length;
-            cols = input[0].Length;
+            int rows = input.Length;
+            int cols = input[0].Length;
 
-            map = new char[rows, cols];
+            char [,] map = new char[rows, cols];
+            (int x, int y) start = (0,0);
 
             // Find guard starting position, and copy input into a mutable 2D char-array
             for (int i = 0; i < rows; i++)
@@ -29,26 +23,33 @@ namespace AdventOfCode
                     map[j, i] = input[i][j];
                     if (input[i][j] != '.' && input[i][j] != '#')
                     {
-                        startX = j;
-                        startY = i;
+                        start.x = j;
+                        start.y = i;
                     }
                 }
+
+            return part switch
+            {
+                1 => CountPositions(map, start),
+                2 => CountPossibleObstructions(map, start),
+                _ => throw new ArgumentException("Not a valid part")
+            };
         }
 
-        public int RunPart1()
+        public int CountPositions(char[,] map, (int x , int y) start)
         {
-            return Move(startX, startY, STRAIGHT, UP).visited.Count();
+            return Move(map, start.x, start.y, STRAIGHT, UP).visited.Count();
         }     
 
-        public int RunPart2()
+        public int CountPossibleObstructions(char[,] map, (int x, int y) start)
         {
             int loops = 0;
-            foreach ((int x, int y) in Move(startX, startY, STRAIGHT, UP).visited.Skip(1))
+            foreach ((int x, int y) in Move(map, start.x, start.y, STRAIGHT, UP).visited.Skip(1))
             {
                 // Place obstacle at every spot in the path once (except start), and test for loops.
                 char old = map[x, y];
                 map[x, y] = '#';
-                loops += Move(startX, startY, STRAIGHT, UP).loop;
+                loops += Move(map, start.x, start.y, STRAIGHT, UP).loop;
                 map[x, y] = old;
             }
 
@@ -57,7 +58,7 @@ namespace AdventOfCode
 
         // Walk the guard starting from a certain position into a certain direction.
         // Ends when guard goes out of bounds, or enters a loop.
-        private (IEnumerable<(int, int)> visited, int loop) Move(int x, int y, int dx, int dy)
+        private (IEnumerable<(int, int)> visited, int loop) Move(char[,] map, int x, int y, int dx, int dy)
         {
             HashSet<(int, int, int, int)> visited = new HashSet<(int, int, int, int)>();
 
@@ -75,7 +76,7 @@ namespace AdventOfCode
                 int nextY = y + dy;
 
                 // Moves out of bounds, done.
-                if (nextX < 0 || nextX >= cols || nextY < 0 || nextY >= rows) break;
+                if (nextX < 0 || nextX >= map.GetLength(0) || nextY < 0 || nextY >= map.GetLength(1)) break;
 
                 // Rotate by 90 degrees if obstacle ahead
                 if (map[nextX, nextY] == '#')
