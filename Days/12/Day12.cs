20b62094ccecd42dfd4781eb5ae9c1cc5fb508f7
larsdekwant <Larskwant@gmail.com>
2024-12-12 23:16:39 +0100

    completed day 12

--- Days/12/Day12.cs
+++ Days/12/Day12.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics.CodeAnalysis;
@@ -13,9 +14,8 @@ namespace AdventOfCode
         private readonly static (int x, int y)[] CornerDirections = [(0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1)];
         private readonly static int UNKNOWN = 0;
 
-        private Dictionary<char, int> RegionsOfType = [];
+        private Dictionary<int, (int area, int perimeter, int sides)> RegionStats = [];
         private int CurrentRegion = 0;
-        private Dictionary<int, (int area, int perimeter)> RegionStats = [];
 
         public int RunPart(int part)
         {
@@ -25,8 +25,7 @@ namespace AdventOfCode
 
             char[,] garden  = new char[rows, cols];
             int[,] regions = new int[rows, cols];
-            this.RegionsOfType = [];
-            this.CurrentRegion = 0;
+            this.CurrentRegion = 1;
             this.RegionStats = [];
 
             // Store garden in 2D array.
@@ -34,118 +33,108 @@ namespace AdventOfCode
                 for (int x = 0; x < cols; x++)
                 {
                     garden[x, y] = input[y][x];
-                    regions[x, y] = UNKNOWN;
-                    RegionsOfType.TryAdd(input[y][x], 0);                   
+                    regions[x, y] = UNKNOWN;            
                 }
 
-            for (int x = 0; x < garden.GetLength(0); x++)
-            {
+            // Gather all required information about the regions.
+            for (int x = 0; x < garden.GetLength(0); x++)           
                 for (int y = 0;y < garden.GetLength(1); y++)
                 {
                     if (regions[x, y] != UNKNOWN) continue;
 
+                    var stats = ExploreRegion(garden, regions, (x,y), (0, 0, 0));
+                    RegionStats.Add(CurrentRegion, stats);
                     CurrentRegion++;
-                    char plant = garden[x, y];
-                    var stats = ExploreRegion(garden, regions, (x,y), 0, 0);
-                    int corners = CountRegionCorners(regions, new bool[rows, cols], (x, y), 0);
-                    //int sides = WalkRegionSides(garden, regions, CurrentRegion, (x, y-1), (x, y-1), (1, 0), 0);
-                    RegionStats.Add(CurrentRegion, (stats.area, corners));
                 }
-            }
+            
 
-            return RegionStats.Values.Sum(region => region.area * region.perimeter);
+            return part switch
+            {
+                1 => RegionStats.Values.Sum(region => region.area * region.perimeter),
+                2 => RegionStats.Values.Sum(region => region.area * region.sides),
+                _ => throw new ArgumentException("Not a valid part")
+            };              
         }
 
-        private (int area, int perimeter) ExploreRegion(char[,] garden, int[,] regions, (int x, int y) start, int area, int perimeter)
+        // Explores the entire region starting from a given tile, returns the area, perimiter and amount of corners of that region.
+        private (int area, int perimeter, int sides) ExploreRegion(char[,] garden, int[,] regions, (int x, int y) tile, (int area, int perimeter, int sides) stats)
         {
-            if(regions[start.x, start.y] == CurrentRegion) return (area, perimeter);
+            if(regions[tile.x, tile.y] == CurrentRegion) return stats;
 
-            regions[start.x, start.y] = CurrentRegion;
-            char plant = garden[start.x, start.y];
-            int extraPerimeter = 4;
-            int extraArea = 1;
-            foreach((int dx, int dy) in CardinalDirections)
-            {
-                (int x, int y) next = (start.x + dx, start.y + dy);
-                if (InBounds(garden, next) && garden[next.x, next.y] == plant)
-                {
-                    extraPerimeter--;
-                    var stats = ExploreRegion(garden, regions, next, area, perimeter);
-                    extraArea += stats.area;
-                    extraPerimeter += stats.perimeter;
-                }
-            }
+            regions[tile.x, tile.y] = CurrentRegion;
 
-            return (area + extraArea, perimeter + extraPerimeter);
-        }
+            int area = 1;
+            int perimeter = 4;
+            int sides = CountCorners(garden, regions, tile);
 
-        private int CountRegionCorners(int[,] regions, bool[,] visited, (int x, int y) start, int corners)
-        {
-            if (visited[start.x, start.y]) return corners;
-
-            int region = regions[start.x, start.y];
-            visited[start.x, start.y] = true;
-            int totalcorners = 0;
-            for (int corner = 0; corner < 4; corner++)
-            {
-                (int dx, int dy)[] cornerChecks = CornerDirections.Skip(2 * corner).Take(3).ToArray();
-                
-                // WIP
-                int isCorner = 0;
-                for (int dir = corner * 2; dir < (corner * 2) + 3; dir++)
-                {
-                    (int dx, int dy) cDir = CornerDirections[dir];
-                    (int x, int y) check = (start.x + cDir.dx, start.y + cDir.dy);
-                    if (InBounds(regions, check) && visited[check.x, check.y])
-                    {
-                        isCorner = 0;
-                        break;
-                    }
-                    if(!InBounds(regions, check) || regions[check.x, check.y] != region) isCorner ++;                   
-                }
-
-                // Fix this, cuz '2' is also a possible corner in the following configuration:
-                // AABB
-                // BBAA
-                if (isCorner == 1 || isCorner == 3) 
-                    totalcorners++;
-            }                
-
+            // Explore all non-diagonal neighbours
             foreach ((int dx, int dy) in CardinalDirections)
             {
-                (int x, int y) next = (start.x + dx, start.y + dy);
-                if (InBounds(regions, next) && regions[next.x, next.y] == region)
-                {                  
-                    totalcorners += CountRegionCorners(regions, visited, next, corners);                    
+                (int x, int y) next = (tile.x + dx, tile.y + dy);
+                if (InBounds(garden, next) && garden[next.x, next.y] == garden[tile.x, tile.y])
+                {
+                    var region = ExploreRegion(garden, regions, next, stats);
+                    area += region.area;
+                    perimeter += region.perimeter;
+                    sides += region.sides;
+
+                    // Had neighbour in the region, so reduce perimeter by 1.
+                    perimeter--;
                 }
             }
 
-            return totalcorners;
+            return (stats.area + area, stats.perimeter + perimeter, sides);
         }
 
-        private int WalkRegionSides(char[,] garden, int[,] regions, int region, (int x, int y) start, (int x, int y) pos, (int dx, int dy) dir, int sides)
+        // Count all corners for a tile, both inward and outward corners
+        private int CountCorners(char[,] garden, int[,] regions, (int x, int y) tile)
         {
-            if (pos == start && sides > 1) return sides;
-
-            (int x, int y) next = (pos.x + dir.dx, pos.y + dir.dy);
-            (int dx, int dy) right = (-dir.dy, dir.dx);
-            (int x, int y) nextRight = (next.x + right.dx, next.y + right.dy);
-
-            // Turn right onto new side
-            if (!InBounds(garden, nextRight) || regions[nextRight.x, nextRight.y] != region)
+            int corners = 0;
+            char plant = garden[tile.x, tile.y];
+            int region = regions[tile.x, tile.y];
+            for (int corner = 0; corner < 4; corner++)
             {
-                return WalkRegionSides(garden, regions, region, start, nextRight, right, sides + 1);
+                // [(0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1)];
+                // Take the 3 directional vectors that determine the quadrant of this corner
+                (int dx, int dy)[] checks = CornerDirections.Skip(2 * corner).Take(3).ToArray();
+
+                // The 3 tiles (clockwise order) on the corner to check
+                (int x, int y) c0 = (tile.x + checks[0].dx, tile.y + checks[0].dy);
+                (int x, int y) c1 = (tile.x + checks[1].dx, tile.y + checks[1].dy);
+                (int x, int y) c2 = (tile.x + checks[2].dx, tile.y + checks[2].dy);
+
+                // If one of the neighbours was already visited, the corner is already accounted for.
+                if (InBounds(garden, c0) && regions[c0.x, c0.y] == region) continue;
+                if (InBounds(garden, c2) && regions[c2.x, c2.y] == region) continue;
+
+                // True if out of bounds or not belonging to this region
+                bool bound0 = !InBounds(garden, c0) || garden[c0.x, c0.y] != plant;
+                bool bound1 = !InBounds(garden, c1) || garden[c1.x, c1.y] != plant;
+                bool bound2 = !InBounds(garden, c2) || garden[c2.x, c2.y] != plant;
+
+                // Any outward corner, including the diagonal corners, for example:
+                //  ...       ..A
+                //  AA.  and  AA.
+                //  .A.       .A.
+                if (bound0 && bound2)
+                {
+                    corners++;
+                }
+                // Any inward corner, for example:
+                //  A.
+                //  AA
+                else if (((bound0 && !bound1 && !bound2)
+                      || (!bound0 && bound1 && !bound2)
+                      || (!bound0 && !bound1 && bound2))
+                      && regions[c1.x, c1.y] != region)
+                {
+                    corners++;
+                }
             }
-            // Turn left onto new side
-            if (InBounds(garden, next) && regions[next.x, next.y] == region)
-            {
-                return WalkRegionSides(garden, regions, region, start, pos, (dir.dy,  -dir.dx), sides + 1);
-            }
-            // Walk forward on same side
-            return WalkRegionSides(garden, regions, region, start, next, dir, sides);
+            return corners;
         }
-
-        private bool InBounds<T>(T[,] garden, (int x, int y) pos) =>
-            pos.x >= 0 && pos.x < garden.GetLength(0) && pos.y >= 0 && pos.y < garden.GetLength(1);  
+      
+        private static bool InBounds<T>(T[,] map, (int x, int y) pos) =>
+            pos.x >= 0 && pos.x < map.GetLength(0) && pos.y >= 0 && pos.y < map.GetLength(1);  
     }        
 }
