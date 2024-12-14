8a4571a5b86a86fe18d2ff7e9b83f6e49dfcfd8f
larsdekwant <Larskwant@gmail.com>
2024-12-14 14:15:59 +0100

    reworked day 14 part 2 to be automatic

--- Days/14/Day14.cs
+++ Days/14/Day14.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Numerics;
 using System.Text.RegularExpressions;
-using System.Drawing;
 
 namespace AdventOfCode
 {
@@ -10,6 +8,8 @@ namespace AdventOfCode
         const int IN_BETWEEN_QUADRANTS = 0;
         const int XMAX = 101;
         const int YMAX = 103;
+
+        private readonly static (int x, int y)[] CardinalDirections = [(0, -1), (1, 0), (0, 1), (-1, 0)];
 
         public int RunPart(int part)
         {
@@ -22,43 +22,81 @@ namespace AdventOfCode
                 robots.Add(new Robot(matches[0], matches[1], matches[2], matches[3]));
             }
 
-            if (part == 1)
+            return part switch
             {
-                int seconds = 100;
-                for (int i = 0; i < seconds; i++)
-                {
-                    robots.ForEach(StepRobot);
-                }
+                1 => GetSafetyFactor(robots, 100),
+                2 => FindChristmasTree(robots),
+                _ => throw new ArgumentException("Not a valid part")
+            };
+        }
 
-                return robots
-                    .Select(GetQuadrant)
-                    .Where(q => q != IN_BETWEEN_QUADRANTS)
-                    .GroupBy(q => q)
-                    .Aggregate(1, (acc, val) => acc * val.Count());
+        private int GetSafetyFactor(List<Robot> robots, int seconds = 100)
+        {
+            for (int i = 0; i < seconds; i++)
+            {
+                robots.ForEach(StepRobot);
             }
 
-            // Search for an image that clearly looks like a christmas tree.
-            if (part == 2)
+            return robots
+                .Select(GetQuadrant)
+                .Where(q => q != IN_BETWEEN_QUADRANTS)
+                .GroupBy(q => q)
+                .Aggregate(1, (acc, val) => acc * val.Count());
+        }
+
+        // Assuming that a christmas tree is made out of a large group of robots that are direct neighbours.
+        private int FindChristmasTree(List<Robot> robots)
+        {
+            int seconds = 0;
+            while (true)
             {
-                // If you can't find it, increase this number.
-                int seconds = 10000;
-                for (int i = 0; i < seconds; i++)
-                {
-                    Bitmap bmp = new Bitmap(XMAX, YMAX);
+                robots.ForEach(StepRobot);
+                if (LargestGroup(robots) >= robots.Count / 4) return seconds + 1;
+                seconds++;
+            }
+        }
 
-                    robots.ForEach(StepRobot);
-                    foreach (Robot r in robots)
-                    {
-                        bmp.SetPixel(r.P.x, r.P.y, Color.White);
-                    }
-                    bmp.Save($"../../../Days/14//Renders/{i + 1}.bmp");
-                }
+        // Finds the size of the largest group of robots that are direct neighbours.
+        private int LargestGroup(List<Robot> robots)
+        {
+            int largest = 0;
 
-                // Good luck searching for the easter egg in your folder.
-                return 0;
+            // Map of robot positions
+            bool[,] map = new bool[XMAX, YMAX];
+            foreach(Robot robot in robots)
+            {
+                map[robot.P.x, robot.P.y] = true;
             }
 
-            throw new ArgumentException("Not a valid part");
+            bool[,] visited = new bool[XMAX, YMAX];
+            for (int x  = 0; x < XMAX; x++)           
+                for (int y = 0; y < YMAX; y++)
+                {
+                    if(visited[x, y]) continue;
+
+                    int count = CountGroup((x, y), map, visited);
+                    if (count > largest) largest = count;
+                }
+            
+            return largest;
+        }
+
+        // Explore an entire group of robots and count its members.
+        private int CountGroup((int x, int y) pos, bool[,] robots, bool[,] visited)
+        {
+            if (pos.x < 0 || pos.x >= XMAX || pos.y < 0 || pos.y >= YMAX) return 0;
+            if (!robots[pos.x, pos.y]) return 0;
+            if (visited[pos.x, pos.y]) return 0;
+
+            visited[pos.x, pos.y] = true;
+
+            int total = 0;
+            foreach((int dx, int dy) in CardinalDirections)
+            {
+                total += CountGroup((pos.x + dx, pos.y + dy), robots, visited);
+            }
+
+            return total + 1;
         }
 
         private void StepRobot(Robot robot) 
